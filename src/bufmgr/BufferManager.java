e9e1188ba65206d760f69e2ca52f717408c643d6
navnishds <60588375+navnishds@users.noreply.github.com>
2021-02-19 02:37:46 -0600

    Makefile

--- src/bufmgr/BufferManager.java
+++ src/bufmgr/BufferManager.java
@@ -307,32 +307,7 @@ public class BufferManager implements GlobalConst {
         System.out.print("Load-based BHR (BHR2): ");
         System.out.printf("%9.5f\n", pageLoadBHR);
         System.out.println("+----------------------------------------+");
-      
-      
-        //sc: set the first 8 page refs to 0 so it wont distort ref count
-        // for (int i = 0; i < 9 ; i++)  {  
-        //   //* System.out.println("+---Before setting it to zero: page, refCount: "+pageRefCount[i][2]+", "+pageRefCount[i][1]);
-        //   pageRefCount[i][1]=0;
-        // }
         
-        // //before sorting, need to compare the FINAL round refcounts and fix it
-        // for (int i = 0; i < pageRefCount.length ; i++) {
-        //     if (pageRefCount[i][0] > pageRefCount[i][1]) pageRefCount[i][1] = pageRefCount[i][0];
-        //     pageRefCount[i][0] = 0;
-        // }
-        // //Sort and print top 5 page references here. uncomment and substitute your code
-        // sortbyColumn(pageRefCount, 1);
-        
-        // System.out.println("The top k referenced pages are:");
-        // System.out.println("       Page No.\t\tNo. of references");
-           
-        // for (int i = 0; i < 10 ; i++)    
-        //   System.out.println("\t"+pageRefCount[i][2]+"\t\t"+pageRefCount[i][1]);
-        
-        // System.out.println("+----------------------------------------+");
-        //* System.out.println("pageRefCount.length: "+pageRefCount.length);
-        // *for (int i = 0; i < pageRefCount.length ; i++)    
-          // *System.out.println("\t"+pageRefCount[i][2]+"\t\t"+pageRefCount[i][1]+"\t\t"+pageRefCount[i][0]);
   }
 
 } // public class BufferManager implements GlobalConst
